1d4a4e1feb4d825b36a89c9692c62785f3bc18b5
Sandra Coburn <sandra_coburn@hotmail.com>
2020-01-13 21:42:55 -0800

    Created First component

---
+++ react-sorting-hat/src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Howart's Sorting Hat App</h1>
+      <button>Start Sorting Process</button>
+
+      <h2>
+        We can’t all go to Hogwarts and plonk the Sorting Hat on our head, but
+        here’s the next best thing – Pottermore’s very own Sorting Experience!
+        Which house do you belong to?
+      </h2>
+      <h3>
+        Are you Gryffindor? Are you Hufflepuff? Are you Slytherin? Are you
+        Ravenclaw?
+      </h3>
+    </div>
+  );
+};
+export default HomePage;
---
+++ react-sorting-hat/src/components/HouseQuestions.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HouseQuestions = () => {
+  return (
+    <div className="spell">
+      <div className="spell-list ">
+        <h2>
+          <span aria-label="sparkles" role="img">
+            ✨
+          </span>
+        </h2>
+        <h3>Type of Spell: </h3>
+      </div>
+    </div>
+  );
+};
+export default HouseQuestions;
